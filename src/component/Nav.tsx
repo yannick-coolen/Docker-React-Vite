efe7b55f8098adc3d16a0707d127295505e1f09d
yannick coolen <yannick-coolen-1991@hotmail.nl>
2023-08-24 16:25:08 +0200

    Update page navigation

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import classes from "./Nav.module.scss";
+
+import {NavLink} from "react-router-dom";
+
+const NAVIGATOR: {
+  id: string;
+  name: string;
+  link: string
+}[] = [
+  { id: "1", name: "Home", link: "/" },
+  { id: "2", name: "About", link: "/about"},
+  { id: "3", name: "Contact", link: "/contact"},
+];
 
 export default function Nav() {
   return (
-    <nav>Nav</nav>
-  )
+    <nav>
+      <div className={classes.navigator}>
+        <div>My App</div>
+        <ul className={classes["nav-menu"]}>
+          {NAVIGATOR.map((data) => (
+            <li key={data.id}>
+              <NavLink to={data.link}>{data.name}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 }
