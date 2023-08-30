c84746d60a266ff976351e37830ecbb6dabe92b1
yannick coolen <yannick-coolen-1991@hotmail.nl>
2023-08-30 16:31:57 +0200

    updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,18 @@
+import Container from "../component/Container";
+import { Body, Head, Table } from "../component/Table";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <Container>
+      <Table bordered whitespace={1}>
+        <Table.Head>
+          <Head.Cell bordered>Hello</Head.Cell>
+        </Table.Head>
+        <Table.Body>
+          <Body.Cell>Name</Body.Cell>
+          <Body.Cell>Yan Nick</Body.Cell>
+        </Table.Body>
+      </Table>
+    </Container>
+  );
 }
