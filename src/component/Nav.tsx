005397560364996ba6df845e0960ebed97af78eb
yannick coolen <yannick-coolen-1991@hotmail.nl>
2023-08-30 16:33:53 +0200

    Updated Nav.tsx

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,15 +1,15 @@
-import classes from "./Nav.module.scss";
+import { NavLink } from "react-router-dom";
 
-import {NavLink} from "react-router-dom";
+import classes from "./style/Nav.module.scss";
 
 const NAVIGATOR: {
   id: string;
   name: string;
-  link: string
+  link: string;
 }[] = [
   { id: "1", name: "Home", link: "/" },
-  { id: "2", name: "About", link: "/about"},
-  { id: "3", name: "Contact", link: "/contact"},
+  { id: "2", name: "About", link: "/about" },
+  { id: "3", name: "Contact", link: "/contact" },
 ];
 
 export default function Nav() {
