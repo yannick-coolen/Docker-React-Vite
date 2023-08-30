42f19f24323708093765480276d839886cdc2aae
yannick coolen <yannick-coolen-1991@hotmail.nl>
2023-08-30 16:33:36 +0200

    updated App.tsx

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,5 +1,8 @@
-import Nav from "../component/Nav";
 import { Outlet } from "react-router-dom";
+
+// Components
+import Nav from "../component/Nav";
+import Container from "../component/Container";
 
 export default function App() {
   return (
@@ -8,7 +11,13 @@ export default function App() {
         <Nav />
       </header>
       <main>
-        <Outlet />
+        <Container size="full-size" borderlineStyle="none">
+          <section>
+            <div style={{ marginTop: "2rem" }}>
+              <Outlet />
+            </div>
+          </section>
+        </Container>
       </main>
     </>
   );
