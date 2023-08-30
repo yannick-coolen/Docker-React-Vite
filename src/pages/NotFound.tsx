8b882dea01cda612cfe3cf2369744185433e390e
yannick coolen <yannick-coolen-1991@hotmail.nl>
2023-08-30 16:32:21 +0200

    Updated NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,30 @@
+// Component
 import Container from "../component/Container";
 
-import classes from './NotFound.module.scss'
+import classes from "./NotFound.module.scss";
 
 export default function NotFound() {
   return (
     <Container textPosition="middle">
-      <h1 className={classes.underline}>Page not found</h1>
-      <p>The page you are looking for might have been removed, <br/> had its name changed, or is temporarily unavailable.</p>
-      <br />
-      <p>Please check the URL for any mistakes or return to the <a href="/">homepage</a>.</p>
+      <h1 className={classes.underline}>Page not found &#128577;</h1>
+      <Container borderlineStyle="none">
+        <div>
+          <p>
+            The page you are looking for might have been removed, <br /> had its
+            name changed, or is temporarily unavailable.
+          </p>
+          <br />
+          <p>
+            Please check the URL for any mistakes or click the button below to
+            return to the homepage.
+          </p>
+        </div>
+      </Container>
+      <div style={{ margin: "1rem" }}>
+        <a href="/" className={classes["navigator-link"]}>
+          Homepage
+        </a>
+      </div>
     </Container>
   );
 }
